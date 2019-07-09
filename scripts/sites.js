b1748011cfd37ad8195157348a0430333715d2fd
William Felker <weftech@gmail.com>
2019-07-09 12:02:40 -0700

    Cleaned

--- scripts/sites.js
+++ scripts/sites.js
@@ -5,7 +5,7 @@
 
 const sites = [
   { url: 'https://tinyfactories.space', title: 'tiny factories', type: 'tribe', author: 'gardener', contact: 'email', rss: 'blank', feed: 'blank' },
-    { url: 'https://ooak.space', title: 'OOAK', type: 'product', author: 'blank', contact: 'blank', rss: 'blank', feed: 'blank' },
-   { url: 'http://indiepreneur.wiki', title: 'indiepreneur', type: 'wiki', author: 'neauoire', contact: 'blank', rss: 'https://wiki.xxiivv.com/links/rss.xml', feed: 'blank' }
+  { url: 'https://ooak.space', title: 'OOAK', type: 'product', author: 'blank', contact: 'blank', rss: 'blank', feed: 'blank' },
+  { url: 'http://indiepreneur.wiki', title: 'indiepreneur', type: 'wiki', author: 'gardener', contact: 'blank', rss: 'blank', feed: 'blank' }
 
 ]
